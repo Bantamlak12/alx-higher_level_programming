ba2c573470c692baffd979f128a1a128a131ebff
Bantamlak <neoluka315@gmail.com>
2022-11-29 21:19:17 +0000

    5-print_comb2.py: Fix the digit and separator at the end

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    elif i == 99:
-        print(i)
+    if i == 99:
+        print("{}".format(i))
     else:
-        print("{}".format(i), end=", ")
+        print("{:02}".format(i), end=", ")
