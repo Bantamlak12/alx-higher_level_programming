20753c47c13e1ea3dd410f5763350f67a9619b14
Bantamlak <neoluka315@gmail.com>
2022-11-30 10:21:32 +0000

    8-concat_edges.py: base code changedvim

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print("""The Zen of Python, by Tim Peters
+print('''
+The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -19,4 +20,5 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!""")
+Namespaces are one honking great idea -- let's do more of those!'
+''')
