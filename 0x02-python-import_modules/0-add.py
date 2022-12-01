260f22f422fbbef10f5c2f092bf006c2215f5e73
Bantamlak <neoluka315@gmail.com>
2022-12-01 12:05:57 +0000

    setting the name to main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
-add = add.add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    import add_0 as add
+    add = add.add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
