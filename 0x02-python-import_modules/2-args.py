4d04b2dea05952b7214d0cafa54924896e507719
Bantamlak <neoluka315@gmail.com>
2022-12-01 13:26:32 +0000

    making it unexecutable when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-args = len(sys.argv) - 1
-if args == 0:
-    print(args, "arguments.")
-elif args == 1:
-    print(args, "argument:")
-    print("{}: {}".format(args, sys.argv[1]))
-else:
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print(args, "arguments.")
+    elif args == 1:
+        print(args, "argument:")
+        print("{}: {}".format(args, sys.argv[1]))
+    else:
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
