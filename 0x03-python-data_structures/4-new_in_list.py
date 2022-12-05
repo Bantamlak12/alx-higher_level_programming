75bae1da04ac01462d32f3539f879c4172c378b5
Bantamlak <neoluka315@gmail.com>
2022-12-05 07:53:51 +0000

    a copy of the original list created

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     list_len = len(my_list) - 1
+    list_orig = my_list.copy()
     list_copy = my_list.copy()
     list_copy[idx] = element
     if idx < 0:
-        return my_list
+        return list_orig
     elif idx > list_len:
-        return my_list
+        return list_orig
     else:
         return list_copy
