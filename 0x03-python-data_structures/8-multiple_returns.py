4079a6ab05c486cb91cc6d37c7cc8c88be2bf9ad
Bantamlak <neoluka315@gmail.com>
2022-12-05 18:21:17 +0000

    returing None if tuple is None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        sentence[0] = None
+        return (len(sentence), None)
     else:
         return (len(sentence), sentence[0])
