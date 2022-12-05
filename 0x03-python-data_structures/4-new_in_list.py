f6f0f1c39a6e7cf17696616fccaa1a24accafb93
Bantamlak <neoluka315@gmail.com>
2022-12-05 08:13:10 +0000

    range checked

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list_len = len(my_list) - 1
-    list_orig = my_list.copy()
     list_copy = my_list.copy()
-    list_copy[idx] = element
-    if idx < 0:
-        return list_orig
-    elif idx > list_len:
-        return list_orig
-    else:
+    if idx >= 0 and idx < len(list_copy):
+        list_copy[idx] = element
         return list_copy
+    else:
+        return my_list
