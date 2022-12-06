52a37a21bd4a77b06ad92d73bb0465b000c4a325
Bantamlak <neoluka315@gmail.com>
2022-12-06 14:30:11 +0000

    variable names changed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,34 +2,33 @@
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: The first node of the list
+ * @head: Head pointer to the first list
  * Return: 0 if it is not a palindrom,
  *		   1 if it is a palindrome
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *h = *head;
+	listint_t *next_node = *head;
+	listint_t *current_node = *head;
 	listint_t *ptr = *head;
-	listint_t *prev = NULL;
-	listint_t *temp = NULL;
+	listint_t *h = *head;
+	listint_t *prev_node = NULL;
+
+	while (next_node != NULL)
+	{
+		next_node = next_node->next;
+		current_node->next = prev_node;
+		prev_node = current_node;
+		current_node = next_node;
+	}
+	*head = prev_node;
+	while (ptr != NULL)
+		ptr = ptr->next;
 
 	while (h != NULL)
-	{
-		temp = h->next;
-		h->next = prev;
-		prev = h;
-		h = temp;
-	}
-	*head = prev;
-	while (ptr != NULL)
-	{
-		ptr = ptr->next;
-	}
-	while (h != NULL)
-	{
 		h = h->next;
-	}
+
 	if (ptr == h || h == NULL)
 		return (1);
 	else
