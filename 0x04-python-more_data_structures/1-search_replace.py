cedd851f4a1bb6ad68ced50dfd2e65c07e413fe9
Bantamlak <neoluka315@gmail.com>
2022-12-07 14:48:09 +0000

    using the variables

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [89 if i == 2 else i for i in my_list]
-    return new
+    return ([replace if i == search else i for i in my_list])
