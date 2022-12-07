5b9b477fe32ab7a72f77fdda7238571ade714fde
Bantamlak <neoluka315@gmail.com>
2022-12-07 21:01:01 +0000

    if checking the dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    return (a_dictionary.update({key: value}))
+    if a_dictionary is not None:
+        return (a_dictionary.update({key: value}))
+    else:
+        return None
