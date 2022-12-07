a32dd12069f1e723d95a48fc1530065c3738d157
Bantamlak <neoluka315@gmail.com>
2022-12-07 21:06:03 +0000

    using subscripting method instead

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None:
-        return (a_dictionary.update({key: value}))
-    else:
-        return None
+    a_dictionary[key] = value
+    return a_dictionary
