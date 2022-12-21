6c3279bc01753d9c9668085b3f0f09146ceb7140
Bantamlak <neoluka315@gmail.com>
2022-12-21 21:45:45 +0000

    new line with a space

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,9 +47,10 @@ class Square:
     def my_print(self):
         """prints a square hash"""
         if self.__size == 0:
-            print("")
+            print("L")
         else:
-            print("")
+            for i in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
                 for j in range(self.__position[0]):
                     print(" ", end="")
