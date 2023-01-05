69d89edd40d5cd750de1ec21b9ced980abbec5e3
Bantamlak <neoluka315@gmail.com>
2023-01-05 21:46:58 +0000

    first commit of the task

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A module that multiplies 2 matrices"""
+
+import numpy as np
+"""Importing library"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies 2 matrices
+
+    Args:
+        m_a (list of lists of int/floats): The first matrix.
+        m_b (list of lists of int/floats): The second matrix.
+
+    Returns:
+        a new matrix
+    """
+
+    return np.dot(m_a, m_b)
