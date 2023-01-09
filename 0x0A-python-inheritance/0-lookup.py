eb12c1cead18f7f48e5931016d1d158150321d3f
Bantamlak <neoluka315@gmail.com>
2023-01-09 16:27:24 +0000

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""A module that returns a ``list`` object"""
+
+
 def lookup(obj):
+    """Returns a list of available attributes and methods of an object"""
+
     return dir(obj)
