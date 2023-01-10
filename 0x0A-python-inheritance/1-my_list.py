e0de7d6006a67e604eb6be851062f57a915b21b8
Bantamlak <neoluka315@gmail.com>
2023-01-10 07:02:27 +0000

    commented

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,5 +2,9 @@
 """A module that defines a class MyList that inherits from list."""
 
 class MyList(list):
+    """Represents a class MyList"""
+
     def print_sorted(self):
+        """prints the list sorted"""
+
         print(sorted(self))
