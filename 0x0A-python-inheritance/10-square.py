2a76f38e81e53b2e7caa7fbbf956dd2415ed4f4f
Bantamlak <neoluka315@gmail.com>
2023-01-10 18:56:11 +0000

    commited

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a new Square"""
+    def __init__(self, size):
+        """Instantiation
+
+        Args:
+            size (int): size of a square
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
