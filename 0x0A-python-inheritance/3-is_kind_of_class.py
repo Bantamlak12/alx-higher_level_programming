e2120c77aaf31ecf884055a0e8c635e0dbc67eaa
Bantamlak <neoluka315@gmail.com>
2023-01-10 08:21:06 +0000

    hot fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,14 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """returns True if the object is an instance of,
-    or if the object is an instance of a class that
-    inherited from, the specified class ; otherwise False"""
+    """Checks an instance's type
 
+    Args:
+        obj (any): object to be checked
+        a_class (type): The class to match the type of obj to.
+
+    Returns:
+        True if the object is an instance of a_class that inherited from,
+        the specified class ; otherwise false.
+    """
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,9 +8,10 @@ def inherits_from(obj, a_class):
     Args:
         obj (any): Object to be checked
         a_class (type): The class to match the type of obj to.
-    
+
     Returns:
         True if the object is an instance of a class that inherited
-        (directly or indirectly) from the specified class ; otherwise False"""
+        (directly or indirectly) from the specified class ; otherwise False
+    """
 
     return issubclass(type(obj), a_class) and type(obj) != a_class
