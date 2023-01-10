6daa840f76cd439df4005c41ed43263b2c30d317
Bantamlak <neoluka315@gmail.com>
2023-01-10 11:55:05 +0000

    hot fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """validates value"""
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(f'{name} must be an integer')
         if value <= 0:
             raise ValueError(f'{name} must be greater than 0')
