c4b4b824b2214cc3b8531977fe950179d3e7d354
Bantamlak <neoluka315@gmail.com>
2023-01-10 09:01:38 +0000

    commited

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represents a BaseGeometry class"""
+
+    def area(self):
+        """Raises a Exception"""
+
+        raise Exception('area() is not implemented')
