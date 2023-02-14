d52b2179edfc200914f5165d2f3cda4097a42aed
Bantamlak <neoluka315@gmail.com>
2023-02-14 23:10:51 +0000

    100-move_to_utf8.sql fixed

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,2 @@
 -- converts hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0` ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
