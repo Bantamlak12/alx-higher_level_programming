7b76ec513c88fcb8d55b3760d32482de520c751d
Bantamlak <neoluka315@gmail.com>
2023-02-14 22:55:24 +0000

    go to utf 8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- converts hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
