9dee61c90843b854d6600cbe9f38f2824e6f7ebe
Bantamlak <neoluka315@gmail.com>
2023-03-13 20:32:27 +0000

    first commit to the task

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log('undefined is undefined');
+} else if (args[2] !== undefined && args[3] !== undefined) {
+  console.log(args[2] + ' is ' + args[3]);
+} else {
+  console.log(args[2] + ' is undefined');
+}
