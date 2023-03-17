b56ecf012c1569fa5a382be9459a4068a30c2916
Bantamlak <neoluka315@gmail.com>
2023-03-17 21:28:42 +0000

    model_state.py: complied with pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,14 +2,15 @@
 """ A module contains the class definition of a State and an
     instance Base = declarative_base().
 """
-from sqlalchemy import  Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """ Define state class
-    
+
     Args:
         __tablename__: name of the table in the database
         id: state's id
