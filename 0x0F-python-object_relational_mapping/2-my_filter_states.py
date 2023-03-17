0cce567ab617b4e30c1c67dacb3ee192bca7cc54
Bantamlak <neoluka315@gmail.com>
2023-03-17 12:36:41 +0000

    2-my_filter_states.py: query changed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}%' \
-                 ORDER BY id ASC".format(state_name_searched))
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'  ORDER \
+                 BY id ASC".format(state_name_searched))
 
     for row in cur.fetchall():
         print(row)
