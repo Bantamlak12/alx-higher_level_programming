5c949041c8483ee5caa103958bcb2775dcf05f6e
Bantamlak <neoluka315@gmail.com>
2023-03-17 09:41:52 +0000

    1-filter_states.py: first commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" A module that lists all states with a name starting
+    with N (upper N) from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """The code should not be executed when imported"""
+
+    mysql_username = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=mysql_username,
+                         passwd=mysql_passwd,
+                         db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * from states hbtn_0e_0_usa \
+                 WHERE name LIKE 'N%' ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
