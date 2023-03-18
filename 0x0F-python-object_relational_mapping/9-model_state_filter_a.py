05c6df735253d9ccf2f09d8fd79745be86a5c417
Bantamlak <neoluka315@gmail.com>
2023-03-18 06:57:15 +0000

    9-model_state_filter_a.py: sorted in ascending order by states.id

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).all()
+    states = session.query(State).filter(State.name.like('%a%')\
+                                         ).order_by(State.id.asc()).all()
 
     for state in states:
         print('{}:{}'.format(state.id, state.name))
