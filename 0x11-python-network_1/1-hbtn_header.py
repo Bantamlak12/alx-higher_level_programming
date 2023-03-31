802853ccd9efc5ae02c826ca72072c406c46259c
Bantamlak <neoluka315@gmail.com>
2023-03-31 14:35:49 +0000

    task 1: first commit

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Displays the value of the X-Request-Id variable.
+import urllib.request
+import sys
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    requested_id = response.getheader('X-Request-Id')
+print(requested_id)
