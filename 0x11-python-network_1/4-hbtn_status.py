b97625683b0dbf91f277a98a47000ca6893eea5c
Bantamlak <bantamlak29@gmail.com>
2023-03-31 10:13:29 -0700

    task 4: first commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+if __name__ == '__main__':
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    content = response.text
+    content_type = type(content)
+
+    print("Body response:")
+    print("\t- type: {}".format(content_type))
+    print("\t- content: {}".format(content))
