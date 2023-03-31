e2e05ebeb9944375d2e814ab1ac14cec3f273977
Bantamlak <bantamlak29@gmail.com>
2023-03-31 14:52:38 -0700

    task 8: first commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if len(sys.argv) == 2:
+        letters = {'q': sys.argv[1]}
+    elif len(sys.argv) == 1:
+        letters = {'q': ""}
+
+    req = requests.post(url, data=letters)
+    data = req.json()
+
+    try:
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
