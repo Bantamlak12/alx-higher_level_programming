d5b582303b3443c4943332f4685385c7daa446bd
Bantamlak <bantamlak29@gmail.com>
2023-03-31 10:52:58 -0700

    task 5: get method used to get the value of the 'X-Request-Id'

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
-    data = response.headers['X-Request-Id']
-    print(data)
+    requested_id = response.headers.get('X-Request-Id')
+    print(requested_id)
