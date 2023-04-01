3b74869af5fc59fa2fd89e1d367a497faa10a7f2
Bantamlak <bantamlak29@gmail.com>
2023-03-31 17:02:58 -0700

    task 100: first commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" List 10 commits (from the most recent to oldest) of the repository
+    “rails” by the user “rails”
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    endpoint = 'https://api.github.com'
+    user = sys.argv[1]
+    repo = sys.argv[2]
+    url = f'{endpoint}/repos/{user}/{repo}/commits'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        commits = response.json()
+        i = 0
+        for commit in commits:
+            if i == 10:
+                break
+            else:
+                print("{}: {}".format(commit['sha'],
+                      commit['commit']['author']['name']))
+            i = i + 1
+    else:
+        print('Error')
