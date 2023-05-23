df4db11ec72de88b15ccf940aa5f2c1374dfd86a
Bantamlak <bantamlak29@gmail.com>
2023-05-23 14:32:34 +0300

    task 0: semistandard compliant

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const args = process.argv.slice(2);
 const fs = require('fs');
 
-fs.readFile (args[0], 'utf-8', (err, data) => {
-    if (err) {
-	console.error(err);
-	return;
-    }
-    console.log(data);
+fs.readFile(args[0], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
