4ca1b4d1631b28a95710bfc84366549727d16459
Bantamlak <bantamlak29@gmail.com>
2023-05-23 14:30:25 +0300

    Initial commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const fs = require('fs');
+
+fs.readFile (args[0], 'utf-8', (err, data) => {
+    if (err) {
+	console.error(err);
+	return;
+    }
+    console.log(data);
+});
